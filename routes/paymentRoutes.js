948161cf627cc6df695190ca9b246ba8cbf04f98
kiarash fayaz <kiarash.fayyaz72@outlook.com>
2025-08-03 19:32:47 +0200

    payment route protected

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const authController = require('../controllers/authController');
 const paymentController = require('../controllers/paymentController');
 
 const router = express.Router();
 
-router.post('/request', paymentController.requestPayment);
+router.post(
+  '/request',
+  authController.protect,
+  paymentController.requestPayment
+);
 router.get('/verify', paymentController.verifyPayment);
-
 
 module.exports = router;
